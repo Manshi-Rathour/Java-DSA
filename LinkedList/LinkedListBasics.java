8d0ad0e1296867059a04734eb755e0dc9990650e
Manshi Rathour <118745353+Manshi-Rathour@users.noreply.github.com>
2024-07-10 09:16:28 +0530

    delete first, delete last & size implemented

--- LinkedList/LinkedListBasics.java
+++ LinkedList/LinkedListBasics.java
@@ -2,12 +2,17 @@ package LinkedList;
 
 public class LinkedListBasics {
      Node head;
+     private int size;
+     LinkedListBasics(){
+         this.size = 0;
+     }
      public class Node {
          String data;
          Node next;
          Node(String data) {
              this.data = data;
              this.next = null;
+             size++;
          }
      }
 
@@ -33,6 +38,39 @@ public class LinkedListBasics {
         currNode.next = newNode;
      }
 
+     // delete from first
+    public void deleteFirst(){
+         if(head == null){
+             System.out.println("List is empty.");
+             return;
+         }
+         size--;
+         head = head.next;
+    }
+
+    // delete from last
+    public void deleteLast(){
+         if(head == null){
+             System.out.println("List is empty.");
+             return;
+         }
+
+         size--;
+
+         if(head.next == null){
+             head = null;
+             return;
+         }
+
+         Node secondLast = head;
+         Node lastNode = head.next;
+         while(lastNode.next != null) {
+             lastNode = lastNode.next;
+             secondLast = secondLast.next;
+         }
+         secondLast.next = null;
+    }
+
      // print list
      public void printList(){
         Node currNode = head;
@@ -44,15 +82,31 @@ public class LinkedListBasics {
          System.out.println("null");
      }
 
+     // get size of LinkedList
+    public int getSize(){
+         return size;
+    }
+
+
     public static void main(String[] args) {
         LinkedListBasics list = new LinkedListBasics();
 
         list.addFirst("a");
         list.addFirst("is");
         list.printList();
+
         list.addLast("list");
         list.printList();
+
         list.addFirst("this");
         list.printList();
+
+        list.deleteFirst();
+        list.printList();
+
+        list.deleteLast();
+        list.printList();
+
+        System.out.println("Size of the LinkedList is " + list.getSize());
     }
 }

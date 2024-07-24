d18e8d9181dfdd32d9230fc82f156b7ed6d34b16
Manshi Rathour <118745353+Manshi-Rathour@users.noreply.github.com>
2024-07-24 09:52:07 +0530

    binary tree

---
+++ Tree/BinaryTrees.java
@@ -0,0 +1,70 @@
+package Tree;
+
+public class BinaryTrees {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree{
+        static int idx = -1;
+        public static Node buildTree(int[] nodes){
+            idx++;
+            if(nodes[idx] == -1){
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+    // preorder traversal
+    public static void preorder(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    // inorder traversal
+    public static void inorder(Node root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+    // postorder traversal
+    public static void postorder(Node root){
+        if(root == null){
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+    }
+    public static void main(String[] args) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println("Root: " + root.data);
+        System.out.println("Preorder Traversal: ");
+        preorder(root);
+        System.out.println();
+        System.out.println("Inorder Traversal: ");
+        inorder(root);
+        System.out.println();
+        System.out.println("Postorder Traversal: ");
+        postorder(root);
+    }
+}

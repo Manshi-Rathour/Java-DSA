77682ea68eceb7a7fc5af773fed3a4793af4e84a
Manshi Rathour <118745353+Manshi-Rathour@users.noreply.github.com>
2024-08-05 10:17:15 +0530

    deleting a node

--- Tree/BinarySearchTree.java
+++ Tree/BinarySearchTree.java
@@ -47,6 +47,41 @@ public class BinarySearchTree {
             return search(root.right, key);
         }
     }
+    // Deleting a node
+    public static Node delete(Node root, int val){
+        if(root.data > val){
+            root.left = delete(root.left, val);
+        }
+        else if(root.data < val){
+            root.right = delete(root.right, val);
+        }
+        else{ // root.data == val
+            // case 1: leaf node
+            if(root.left == null && root.right == null){
+                return null;
+            }
+
+            // case 2: one child
+            if(root.left == null){
+                return root.right;
+            }
+            else if(root.right == null){
+                return root.left;
+            }
+
+            // case 3: 2 children
+            Node IS = inorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+        return root;
+    }
+    public static Node inorderSuccessor(Node root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
     public static void main(String[] args) {
         int[] values = {5, 1, 3, 4, 2, 7};
         Node root = null;
@@ -64,5 +99,8 @@ public class BinarySearchTree {
         else{
             System.out.println("Not found");
         }
+
+        delete(root, 4);
+        inorder(root);
     }
 }

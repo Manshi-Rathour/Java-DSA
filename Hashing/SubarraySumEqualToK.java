0fb5af6360c0eaa27003c449f23b29d78da302c8
Manshi Rathour <118745353+Manshi-Rathour@users.noreply.github.com>
2024-08-22 09:44:40 +0530

    subarray sum equal to k

--- Hashing/SubarraySumEqualToK.java
+++ Hashing/SubarraySumEqualToK.java
@@ -1,2 +1,32 @@
-package Hashing;public class SubarraySumEqualToK {
+package Hashing;
+
+import java.util.HashMap;
+
+public class SubarraySumEqualToK {
+    public static void main(String[] args) {
+        int[] arr = {10, 2, -2, -20, 10};
+        int k = -10;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int sum = 0;
+        int ans = 0;
+
+        map.put(0,1);
+
+        for(int j=0; j<arr.length; j++){
+            sum += arr[j];
+
+            if(map.containsKey(sum-k)){
+                ans += map.get(sum-k);
+            }
+            if(map.containsKey(sum)){
+                map.put(sum, map.get(sum) + 1);
+            }
+            else{
+                map.put(sum, 1);
+            }
+        }
+        System.out.println(ans);
+    }
 }
